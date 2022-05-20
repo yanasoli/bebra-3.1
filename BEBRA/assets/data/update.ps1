d0e697a66c5dc2341f02d71ec6f2cc7236f952c9
yanasoli <105938238+yanasoli@users.noreply.github.com>
2022-05-20 21:41:43 +0300

    first

---
+++ BEBRA/assets/data/update.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/npc/items.json -OutFile items.json
+Invoke-WebRequest https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/npc/npc_abilities.json -OutFile npc_abilities.json
+Invoke-WebRequest https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/npc/npc_heroes.json -OutFile npc_heroes.json
+Invoke-WebRequest https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/npc/npc_units.json -OutFile npc_units.json
